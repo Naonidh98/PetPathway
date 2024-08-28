700eeba8231bd24d4de50d6502a75b1261b9c5cd
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2024-08-28 22:07:25 +0530

    Breeds page created

---
+++ frontend/src/assets/BreedData.js
@@ -0,0 +1,48 @@
+import GoldenRetriever from "./../assets/goldenRetriever.png";
+import Pug from "./../assets/pug.png";
+import Beagle from "./../assets/beagle.png";
+import GermanShepherd from "./../assets/germanShepherd.png";
+import Dachshund from "./../assets/dachshund.png";
+import Boxer from "./../assets/boxer.png";
+import Doberman from "./../assets/doberman.png";
+import Rottweiler from "./../assets/rottweiler.png";
+import Pomeranian from "./../assets/pomeranian.png";
+
+export default [
+    {
+        name: "Golden Retriever",
+        image: GoldenRetriever,
+    },
+    {
+        name: "Beagle",
+        image: Beagle,
+    },
+    {
+        name: "Pug",
+        image: Pug,
+    },
+    {
+        name: "German Shepherd",
+        image: GermanShepherd,
+    },
+    {
+        name: "Dachshund",
+        image: Dachshund,
+    },
+    {
+        name: "Boxer",
+        image: Boxer,
+    },
+    {
+        name: "Doberman",
+        image: Doberman,
+    },
+    {
+        name: "Rottweiler",
+        image: Rottweiler,
+    },
+    {
+        name: "Pomeranian",
+        image: Pomeranian,
+    },
+];
